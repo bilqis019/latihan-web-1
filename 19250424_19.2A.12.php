b1204e7f73832f888f64a48a74624195fdad6b5b
unknown <19250424@bsi.ac.id>
2026-05-05 11:24:46 +0700

    nim genap

---
+++ 19250424_19.2A.12.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php include 'nav.php'; ?>
+
+<div style="padding-left:16px">
+  <center>
+    <h2>Biodata</h2>
+    <form method="post" action="" class="mb-4">
+<table style="width:40%; border-collapse: collapse;" border="1">
+    <tr>
+      <td>NIM</td>
+      <td>:</td>
+      <td><input type="number" name="nim" required></td>
+    </tr>
+      <tr>
+      <td>Nama</td>
+      <td>:</td>
+      <td><input type="text" name="nama" placeholder="Masukkan Nama Anda" required></td>
+    </tr>
+    <tr>
+    <td>Kelas</td>
+      <td>:</td>
+      <td><input type="text" name="kelas" placeholder="Masukkan Kelas Anda" required></td>
+    </tr>
+    <tr>
+    <td>Jurusan</td>
+      <td>:</td>
+      <td><input type="text" name="jurusan" placeholder="Masukkan Jurusan Anda" required></td>
+    </tr>
+    <tr>
+    <td>Matakuliah</td>
+      <td>:</td>
+      <td><input type="text" name="matakuliah" placeholder="Masukkan Matakuliah Anda" required></td>
+    </tr>
+      <td><button type="submit" name="kirim">kirim</button></td>
+    </tr>
+    </table>
+    </form>
+       <?php if (isset($_POST['kirim'])): ?> 
+    <strong>NIM:</strong> <?php echo $_POST['nim'] ? $_POST['nim'] : ''; ?><br>
+    <strong>Nama:</strong> <?php echo $_POST['nama'] ? $_POST['nama'] : ''; ?><br>
+    <strong>Kelas:</strong> <?php echo $_POST['kelas'] ? $_POST['kelas'] : ''; ?><br>
+    <strong>Jurusan:</strong> <?php echo $_POST['jurusan'] ? $_POST['jurusan'] : ''; ?><br> 
+    <strong>Matakuliah:</strong> <?php echo $_POST['matakuliah'] ? $_POST['matakuliah'] : ''; ?><br>
+    <?php endif; ?>
+</center>
+</div>
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
+
+</body>
+</html>
